cb98ba70669ae41586d3c67fec757d73aad30876
fedeiada <fede.iada96@gmail.com>
2025-02-04 17:14:54 +0100

    expanded opt_vect

--- distributed_ho_mpc/distributed_ho_mpc/network_simulation.py
+++ distributed_ho_mpc/distributed_ho_mpc/network_simulation.py
@@ -1,231 +1,198 @@
-import numpy as np
-import copy
-import time
-from Message import *
+import numpy as np 
+import networkx as nx
+from matplotlib.animation import FuncAnimation
+import matplotlib.pyplot as plt
+from Node import Node
 from robot_models import get_unicycle_model, get_omnidirectional_model
-import casadi as ca
+import settings as st
+from auxiliary.disp_het_multi_rob import display_animation
 
-from auxiliary.evolve import evolve
-from ho_mpc.ho_mpc import HOMPC
-from ho_mpc.ho_mpc_multi_robot import HOMPCMultiRobot, TaskType
-from ho_mpc.tasks_creator_ho_mpc_mr import TasksCreatorHOMPCMultiRobot 
+model = {
+   'unicycle': get_unicycle_model(st.dt),
+   'omnidirectional': get_omnidirectional_model(st.dt)
+}
 
+# ---------------------------------------------------------------------------- #
+#                                 Task scheduler                               #
+# ---------------------------------------------------------------------------- #
 
-class Node():
-    """
-    Representing the node and its actions and attributes
-
-    Class method:
-    -> transmit_data: create a message with information to share with neighbours
-    -> update: pop from local memory buffer received messages and update its state
-    -> receive_data: append the received message in a local memory buffer
-    
-    """
-
-    def __init__(self, node_id: int, adjacency_vector: np.array, model: str, dt: float, tasks: list, goals: np.array, n_steps: int):
-
-        super(Node, self).__init__()
-
-        self.node_id = node_id  # ID of the node
-        self.adjacency_vector = adjacency_vector # neighbours
-        self.neigh = np.nonzero(adjacency_vector)[0].tolist() # index of neighbours
-        self.degree = len(self.neigh) # numbers of neighbours
-         
-        self.x_init = np.random.randint(100) # initial state
-        self.xi = self.x_init # state of the agent
-        self.x_past = [] # evolution of the past states
-        self.neighbors_sum = 0.0 # local neighbors_sum
-
-        self.buffer = [] # local buffer to receive messages
+goals = [
+        np.array([13, -16]),
+        np.array([11, 6]),
         
-        # ======================== Define The System Model ======================= #
-    
-        # Define the state and input variables, and the discrete-time dynamics model.
-        
-        self.dt = dt       # timestep size
-        
-        self.s = [None for _ in range(2)]
-        self.u = [None for _ in range(2)]
-        self.s_kp1 = [None for _ in range(2)]
+    ]
 
-        self.goals = goals
+system_tasks = {
+    'agent_0': [{'prio':1,             # priority
+                 'name':"input_limits",   # task type
+                 'Xsym' : None
+                      },
+                {'prio':2,             # priority
+                 'name':"input_smooth",   # task type
+                 'Xsym' : None
+                      },
+                 {'prio':3,             # priority
+                 'name':"formation",   # task type
+                 'Xsym' : [[('u2',2),('s2',3)],[('u1',2),('s1',3)]]
+                      },
+                {'prio':4,             # priority
+                 'name':"position",   # task type
+                 'goal': goals[0],         # [x,y] 
+                 'goal_index':0,          # index of the corrisponding list goal's element 
+                 'Xsym': [[('u1',2),('s1',3)],[('u2',2),('s2',3)]]
+                        },       
+                {'prio':5,             # priority
+                 'name':"position",   # task type
+                'goal': goals[1],         # [x,y]
+                'goal_index':1,           # index of the corrisponding list goal's element 
+                'Xsym' : [[('u2',2),('s2',3)],[('u1',2),('s1',3)]]
+                      }
+                ],
+    'agent_1': [{'prio':1,             # priority
+                 'name':"input_limits",   # task type
+                 'Xsym' : None
+                      },
+                {'prio':2,             # priority
+                 'name':"input_smooth",   # task type
+                 'Xsym' : None
+                      },
+                {'prio':3,             # priority
+                 'name':"formation",   # task type
+                 'Xsym' : [[('u2',2),('s2',3)],[('u1',2),('s1',3)]]
+                      },
+                {'prio':5,             # priority
+                 'name':"position",   # task type
+                 'goal': goals[1],         # [x,y] 
+                 'goal_index':1,          # index of the corrisponding list goal's element 
+                 'Xsym': [[('u1',2),('s1',3)],[('u2',2),('s2',3)]]
+                        }  
+                ],
+    'agent_2': [{'prio':1,             # priority
+                 'name':"input_limits",   # task type
+                 'Xsym' : None
+                      },
+                {'prio':2,             # priority
+                 'name':"input_smooth",   # task type
+                 'Xsym' : None
+                      },
+                {'prio':3,             # priority
+                 'name':"position",   # task type
+                 'goal': goals[1],         # [x,y] 
+                 'goal_index':2,          # index of the corrisponding list goal's element 
+                 'Xsym': [[('u1',2),('s1',3)],[('u2',2),('s2',3)]]
+                        },
+                  {'prio':4,             # priority
+                 'name':"position",   # task type
+                 'goal': goals[0],         # [x,y] 
+                 'goal_index':2,          # index of the corrisponding list goal's element 
+                 'Xsym': [[('u1',2),('s1',3)],[('u2',2),('s2',3)]]
+                        }     
+                ],
+    'agent_3': [{'prio':1,             # priority
+                 'name':"input_limits",   # task type
+                      },
+                {'prio':2,             # priority
+                 'name':"input_smooth",   # task type
+                      },
+                {'prio':3,             # priority
+                 'name':"position",   # task type
+                'goal': goals[0],         # [x,y]
+                'goal_index':0, 
+                      },
+                {'prio':4,             # priority
+                 'name':"position",   # task type
+                'goal': goals[1],         # [x,y]
+                'goal_index':0, 
+                      }
+                ],
+}
 
-        self.s[0], self.u[0], self.s_kp1[0] = get_unicycle_model(self.dt)
-        self.s[1], self.u[1], self.s_kp1[1] = get_omnidirectional_model(self.dt)
+# ---------------------------------------------------------------------------- #
+#               Create the network and connection between agents               #
+# ---------------------------------------------------------------------------- #
 
-        #self.s, self.u, self.s_kp1 = model
-        self.n_steps = n_steps
-        self.step = 0
-        self.tasks = tasks
 
-        self.Xsym = [task['Xsym'] for task in self.tasks]
-         
-        self.s_next = [[np.array([0,0,0]),np.array([0,0,0])]]
-        self.Z_old = []
-        self.Z_neigh = {f'{i}': [[np.eye(20)]] for i in self.neigh} #np.empty([20,20])
+# deterministic graphs = evolve(s, u_star, dt)
+if not st.random_graph:
+    graph_matrix = np.array([[0.,1.,1.,0.],
+                            [1.,0.,1.,0.],
+                            [1.,1.,0.,1.],    
+                            [0.,0.,1.,0.]])
+    network_graph = nx.from_numpy_array(graph_matrix, nodelist = [0,1,2,3])
 
-    
-    def receive_data(self, message)->None:
-        " Append the received information in a local buffer"
 
-        self.buffer.append(message)
+# random graph 🎲
+while st.random_graph:
+    network_graph = nx.gnp_random_graph(st.n_nodes, st.p)
+    graph_matrix = nx.to_numpy_array(network_graph)
 
-    # ---------------------------------------------------------------------------- #
-    #                                     Task                                     #
-    # ---------------------------------------------------------------------------- #
-    def Tasks(self)->None:
-        # Define the tasks separately.
+    test = np.linalg.matrix_power((st.I_NN+graph_matrix),st.n_nodes)
 
-        n_robots = [self.degree+1, 0] # n° of neighbours + self agent
+    if np.all(test>0):
+        print("the graph is connected")
+        #nx.draw(network_graph)
+        #plt.show()
+        break 
+    else:
+        print("the graph is NOT connected")
 
-        tasks_creator = TasksCreatorHOMPCMultiRobot(
-                                                         self.s, self.u, self.s_kp1, self.dt, n_robots,
-                                                        )
-        
-        self.task_input_limits = tasks_creator.get_task_input_limits()
-        self.aux, self.mapping, self.task_formation, self.task_formation_coeff = tasks_creator.get_task_formation()
+# ---------------------------------------------------------------------------- #
+#         Create agents and initialize them based on settings and tasks        #
+# ---------------------------------------------------------------------------- #
 
-        self.task_pos       = [None for i in range(len(self.goals))]
-        self.task_pos_coeff = [None for i in range(len(self.goals))]
-        for i, g in enumerate(self.goals):
-            self.task_pos[i], self.task_pos_coeff[i] = tasks_creator.get_task_pos_ref(
-                [[g for n_j in range(n_robots[c])] for c in range(len(n_robots))]
-        )
-        self.task_input_smooth, self.task_input_smooth_coeffs = tasks_creator.get_task_input_smooth()
-        self.task_input_limits_coeffs = [
-            np.array([0, 0, 0, 0])
-        ]
-        
-        self.task_vel_reference = ca.vertcat(
-            (self.s_kp1[0] - self.s[0]) / self.dt - 1,
-            (self.s_kp1[1] - self.s[1]) / self.dt - 0
-        )
-            
-        self.task_input_min = tasks_creator.get_task_input_min()
 
-    # ---------------------------------------------------------------------------- #
-    #                                      MPC                                     #
-    # ---------------------------------------------------------------------------- #
-    def MPC(self)->None:
-        self.hompc = HOMPCMultiRobot(self.s, self.u, self.s_kp1, [self.degree+1,0])
-        self.hompc.n_control = 4
-        self.hompc.n_pred = 0
-        
-        for task in self.tasks:
-            if task['name'] == "input_limits":
-                self.hompc.create_task(
-                                name = "input_limits", prio = task['prio'],
-                                type = TaskType.Same,
-                                ineq_task_ls = self.task_input_limits,
-                                #ineq_task_coeff= self.task_input_limits_coeffs
-                                )
-            elif task['name'] == "position":
-                self.hompc.create_task(
-                                name = "position", prio = task['prio'],
-                                type = TaskType.Same,
-                                eq_task_ls = self.task_pos[task['goal_index']],
-                                eq_task_coeff = self.task_pos_coeff[task['goal_index']],
-                                time_index = [0, 1, 2, 3]
-                                )
-            elif task['name'] == "input_minimization":
-                self.hompc.create_task(
-                                name = "input_minimization", prio = task['prio'],
-                                eq_task_ls = self.task_input_min,
-                                )          
-            elif task['name'] == "reference":
-                self.hompc.create_task(
-                                name = "reference", prio = task['prio'],
-                                eq_task_ls = self.task_vel_reference,
-                                )
-            elif task['name'] == 'input_smooth':
-                self.hompc.create_task(
-                                name = "input_smooth", prio = task['prio'],
-                                type = TaskType.SameTimeDiff,
-                                ineq_task_ls = self.task_input_smooth,
-                                ineq_task_coeff = self.task_input_smooth_coeffs
-                                        )
-            elif task['name'] == 'formation':
-                self.hompc.create_task_bi(
-                                name = "formation", prio = task['prio'],
-                                type = TaskType.Bi,
-                                aux = self.aux,
-                                mapping = self.mapping,
-                                eq_task_ls = self.task_formation,
-                                eq_task_coeff = self.task_formation_coeff,
-                                        )
+# shared list to communicate state with the neighbours (not used)
+buffer_states = [0, 0, 0, 0]
 
-        # ======================================================================== #
-        
-        self.s = [[np.array([0,0,0]),np.array([0,0,0])]]
-        
-        
-        
-        self.s_history = [None] * self.n_steps
+nodes = [] # list of agents of the system
 
-        return
-    
+#Create an agents of the same type for each node of the system
+for i in range(st.n_nodes):
+    node = Node(i,                          # ID
+                graph_matrix[i],            # Neighbours
+                model['unicycle'],          # robot model
+                st.dt,                         # time step
+                system_tasks[f'agent_{i}'],  # agent's tasks
+                goals,                      #
+                st.n_steps                  #
+                    )
+    nodes.append(node)
 
-    def transmit_data(self):
-        " Create a message with state and the neighbours to share with"
-        
-        if self.step < 2:
-            message = Message(self.node_id, self.xi, self.s_next, Z=None, Xsym=None)
-        else :
-            message = Message(self.node_id, self.xi, self.s_next, self.Z_old[-1], Xsym=None)
-        
+    # create frameworks for the agents
+    nodes[i].Tasks()
+    nodes[i].MPC()
 
-        return message, self.neigh
 
-    
-    def update(self):          
-        """Pop from local buffer the receive states of neighbours and update its value"""
-        
-        self.x_past.append(self.xi)
+# ---------------------------------------------------------------------------- #
+#     iterate through the nodes, transmitting datas and the receiving them     #
+# ---------------------------------------------------------------------------- #
 
-        self.neighbors_sum = self.xi
-        for j in self.neigh:
-            data = self.buffer.pop()
-            self.neighbors_sum += data.node_xi
-            self.s[0][1] = data.s[0][1]
-            if self.step >= 3 :
-                self.null_sharing(data.Z, data.node_id)
+for i in range(st.n_steps):
+    for j in range(st.n_nodes):
+        msg, nn = nodes[j].transmit_data()      # Transmit state to shared memory
+        for jj in nn:
+            nodes[jj].receive_data(msg)         # Neighbours receive data 
+    for j in range(st.n_nodes):
+        nodes[j].update()    # Update state evolution
 
-            
-         
 
-        # update the local state
-        self.xi = self.neighbors_sum/(self.degree + 1)
+# ---------------------------------------------------------------------------- #
+#                          plot the states evolutions                          #
+# ---------------------------------------------------------------------------- #
 
-        if self.step < self.n_steps:
-            print(self.step)
-            
-            self.u_star, self.s_next, Z= self.hompc(copy.deepcopy(self.s), self.Z_neigh)
-            self.Z_old.append(Z)
-                    
-            self.s = evolve(self.s, self.u_star, self.dt)                      
-            '''for i in range(10):
-                self.s = self.s + self.dt / 10 * np.array([
-                    self.u_star[0] * np.cos(self.s[0][2]),
-                    self.u_star[0] * np.sin(self.s[0][2]),
-                    self.u_star[1]
-                ])'''
-            
-            print(self.s)
-            print(self.u_star)
-            print()
-            
-            self.s_history[self.step] = copy.deepcopy(self.s)
-            #self.s_history[self.step, :] = copy.deepcopy(self.s)
-            self.step += 1
-        
-        if self.step > 2:
-            self.hompc.null_consensus_start()
-                
-        return 
-    
-    def null_sharing(self, Z, i):
-        self.Z_neigh[f'{i}'].append(Z)
 
-    def s_update(self, s_neigh, i):
-        # TODO 
-        print('da fare')
+s_hist_merged = [
+    [
+        sum((node.s_history[i][j][:1] for node in nodes), [])
+        for j in range(len(nodes[0].s_history[i]))
+    ]
+    for i in range(len(nodes[0].s_history))
+]
+#s_hist_merged = [ [[s_hist_merged[0][0],s_hist_merged[0][1]], np.array([0,0,0])] for i in s_hist_merged]
+#s_hist_merged = [nodes[0].s_history[i][0:1] + nodes[1].s_history[i][0:1] for i in range(len(nodes[0].s_history))]
+
+
+# display_animation(nodes[0].s_history, goals, None, st.dt, st.visual_method)
+# display_animation(nodes[1].s_history, goals, None, st.dt, st.visual_method)
+display_animation(s_hist_merged, goals, None, st.dt, st.visual_method, show_voronoi=False)
+

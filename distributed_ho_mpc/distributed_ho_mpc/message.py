020c59f4d91a7b631de02bf02c8ceb57eae2486b
Davide De Benedittis <davide.debenedittis@gmail.com>
2025-04-15 14:16:11 +0000

    Changed message structure

--- distributed_ho_mpc/distributed_ho_mpc/message.py
+++ distributed_ho_mpc/distributed_ho_mpc/message.py
@@ -1,19 +1,45 @@
 from dataclasses import dataclass
+import numpy as np
 
 @dataclass
 class Message:
-    """Representing the message with primal variables that should be passed between nodes. This class is a simple data-holder which
-    contains the required data to be transferred between nodes. """
+    sender_id: int
+    x_i: np.ndarray
+    x_j: np.ndarray
+    rho_i: np.ndarray
+    rho_j: np.ndarray
 
-    node_id: int    # Identifier of the agent that send the message
-    x_i: list # state vector
-    x_j: list # s+u
+# TODO: support multiple priorities.
+class MessageSender:
+    """TODO"""
     
-@dataclass
-class Message_dual:
-    """Representing the message with dual variables that should be passed between nodes. This class is a simple data-holder which
-    contains the required dual variables to be transferred between nodes. """
+    def __init__(
+        self,
+        sender_id: int,
+        adjacency_vector: np.ndarray,
+        y: np.ndarray,
+        rho: np.ndarray,
+        n_xi: int
+    ):
+        self.sender_id = sender_id
+        self.adjacency_vector = adjacency_vector
+        
+        self.y = y
+        self.rho = rho
+        
+        self.n_xi = n_xi
+        
+    def send_message(self, receiver_id: int) -> Message:
+        """TODO"""
+        
+        #! This assumes that all the x_i have the same size
+        
+        x_i = self.y[0:self.n_xi]
+        rho_i = self.rho[0:self.n_xi]
+        
+        receiver_idx = list(self.adjacency_vector).index(receiver_id)
+        x_j = self.y[receiver_idx * self.n_xi: (receiver_idx + 1) * self.n_xi]
+        rho_j = self.rho[receiver_idx * self.n_xi: (receiver_idx + 1) * self.n_xi]
+        
+        return Message(self.sender_id, x_i, x_j, rho_i, rho_j)
     
-    node_id: int    # Identifier of the agent that send the message
-    rho_j: list     # dual variable
-    j: int          # id of the node to send the message

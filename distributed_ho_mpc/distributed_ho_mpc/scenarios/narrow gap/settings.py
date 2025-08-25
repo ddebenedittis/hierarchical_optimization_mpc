2a4f086d79e103d17638046f97ed1a79380d9342
fedeiada <fede.iada96@gmail.com>
2025-08-25 07:08:56 +0000

    narrow gap scenario added

---
+++ distributed_ho_mpc/distributed_ho_mpc/scenarios/narrow gap/settings.py
@@ -0,0 +1,46 @@
+import numpy as np
+
+# ---------------------------------------------------------------------------- #
+#                               Network settings                               #
+# ---------------------------------------------------------------------------- #
+p = 1   # probability of arc of communication
+n_nodes = 4 # numbers of nodes
+random_graph = False    # create a random graph or not
+I_NN = np.identity(n_nodes, dtype=int)
+
+dt = 0.05
+n_steps = 500
+
+communication_range = 6
+
+v_max = 1.8
+v_min = -0.9
+
+# ---------------------------------------------------------------------------- #
+#                              Flags for simulation                            #
+# ---------------------------------------------------------------------------- #
+output = {
+    'display':'plot', 
+    'save': 'save', 
+    'nothing' :'none'
+}
+visual_method = output['display'] # change the key to decide the output visualization
+save_data = True
+simulation = True
+inner_plot = False # plot the inner state of the robots
+estimation_plotting = False
+# ---------------------------------------------------------------------------- #
+#                                 MPC settings                                 #
+# ---------------------------------------------------------------------------- #
+n_control = 1 # mpc control step
+n_pred = 0 # mpc prediction step
+
+n_xi = n_control * 2
+
+# ---------------------------------------------------------------------------- #
+#                                 PDD settings                               #
+# ---------------------------------------------------------------------------- #
+
+
+n_priority = 2
+step_size = 1e-6
